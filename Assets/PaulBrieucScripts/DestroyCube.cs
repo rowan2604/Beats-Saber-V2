66b134ebfed5c47adf0962ec6eb94f48d4365bcf
awildpizza <paul.snauwaert@isen.yncrea.fr>
2022-02-08 14:46:58 +0100

    8/2.2

--- Assets/PaulBrieucScripts/DestroyCube.cs
+++ Assets/PaulBrieucScripts/DestroyCube.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class DestroyCube : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider collider)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (collider.CompareTag("Cube"))
+            Destroy(collider.gameObject);
     }
 }
