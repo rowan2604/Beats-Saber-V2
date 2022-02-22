55b06492418d7c5d3fd8a187103e3c908a013485
awildpizza <paul.snauwaert@isen.yncrea.fr>
2022-02-22 13:49:14 +0100

    22/02

--- Assets/PaulBrieucScripts/DestroyCube.cs
+++ Assets/PaulBrieucScripts/DestroyCube.cs
@@ -6,7 +6,22 @@ public class DestroyCube : MonoBehaviour
 {
     private void OnTriggerEnter(Collider collider)
     {
+        if (collider.CompareTag("CorrectSurface"))
+            Goodhit();
+        else if(collider.CompareTag("WrongSurface"))
+            Badhit();
+
         if (collider.CompareTag("Cube"))
             Destroy(collider.gameObject);
+    }
+
+    private void Goodhit()
+    {
+        Debug.Log("GoodHit");
+    }
+
+    private void Badhit()
+    {
+        Debug.Log("BadHit");
     }
 }
