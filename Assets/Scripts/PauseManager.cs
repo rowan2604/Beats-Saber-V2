513be4332751c8e1710679bb706dce790af70d39
awildpizza <paul.snauwaert@isen.yncrea.fr>
2022-02-25 17:06:40 +0100

    Update PauseManager.cs

--- Assets/Scripts/PauseManager.cs
+++ Assets/Scripts/PauseManager.cs
@@ -10,6 +10,8 @@ public class PauseManager : MonoBehaviour
     [SerializeField] private GameObject _PauseUI;
     [SerializeField] private GameObject _Blocks;
 
+    private bool _PreviousScriptState;
+
     private void Awake()
     {
         _setPause = new Pause();
@@ -28,6 +30,7 @@ public class PauseManager : MonoBehaviour
 
     private void PauseGame(InputAction.CallbackContext context)
     {
+        _PreviousScriptState = GetComponent<CubePlacement>().isActiveAndEnabled;
         GetComponent<CubePlacement>().enabled = false;
         _PauseUI.SetActive(true);
         _Blocks.SetActive(false);
@@ -35,6 +38,8 @@ public class PauseManager : MonoBehaviour
 
     public void ResumeGame()
     {
-
+        GetComponent<CubePlacement>().enabled = _PreviousScriptState;
+        _PauseUI.SetActive(false);
+        _Blocks.SetActive(true);
     }
 }
