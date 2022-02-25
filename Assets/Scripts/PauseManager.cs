d97e79c5682adc4ca963d485d2d0e633e5ccc26b
awildpizza <paul.snauwaert@isen.yncrea.fr>
2022-02-25 16:36:29 +0100

    pause

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PauseManager : MonoBehaviour
+{
+    private Pause _setPause;
+
+    [SerializeField] private GameObject _PauseUI;
+    [SerializeField] private GameObject _Blocks;
+
+    private void Awake()
+    {
+        _setPause = new Pause();
+    }
+
+    private void OnEnable()
+    {
+        _setPause.PauseMap.LaunchPause.performed += PauseGame;
+        _setPause.PauseMap.LaunchPause.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _setPause.PauseMap.LaunchPause.Disable();
+    }
+
+    private void PauseGame(InputAction.CallbackContext context)
+    {
+        _PauseUI.SetActive(true);
+        _Blocks.SetActive(false);
+    }
+}
